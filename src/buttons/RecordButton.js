b2e80721bda3578ffcebd0dc09088179b61c38a7
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-07-07 19:21:55 -0700

    add bone structure to record button

---
+++ src/buttons/RecordButton.js
@@ -0,0 +1,31 @@
+import { Container, Row, Col, Button } from "react-bootstrap";
+import './RecordButton.css';
+
+/**
+ * RecordButton
+ *
+ * Starts audio recording
+ *
+ * props:
+ *    - record: fn to start recording audio
+ *
+ * state:
+ *    - none
+ *
+ * {StartPage, QuestionsPage} => RecordButton
+ */
+function RecordButton({record}) {
+  return (
+    <Container className="RecordButton">
+    <Row>
+    <Col>
+    <Button onClick={record}>
+    Start Assistant
+    </Button>
+    </Col>
+    </Row>
+    </Container>
+  );
+}
+
+export default RecordButton;
